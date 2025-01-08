838e21a6a692717641756e05b977d03b6ddcac3f
romansaborido <roman.saborido@iesnervion.es>
2025-01-08 10:14:43 +0100

    Terminar Ejercicio 20

---
+++ Tema3.2/src/tema3_2/Ejercicio20.java
@@ -0,0 +1,17 @@
+package tema3_2;
+
+public class Ejercicio20 {
+
+	public static void main(String[] args) {
+		
+		// 
+
+	}
+	
+	static String[] secuenciaCuatroLetras(String palabra) {
+		
+		String secuencia[] = new String[0];
+		
+		return secuencia;
+	}
+}
