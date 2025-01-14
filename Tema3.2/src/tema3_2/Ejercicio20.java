da8380fde1784fbfb582a982b6331e3b55ca9c6e
romansaborido <roman.saborido@iesnervion.es>
2025-01-14 14:34:22 +0100

    Ejercicios terminados

--- Tema3.2/src/tema3_2/Ejercicio20.java
+++ Tema3.2/src/tema3_2/Ejercicio20.java
@@ -1,17 +1,54 @@
 package tema3_2;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Ejercicio20 {
 
 	public static void main(String[] args) {
 		
-		// 
+		// Creamos la variable palabra para almacenar la palabra que introduzca el usuario
+		String palabra;
+		
+		// Creamos el array de cadenas secuenciaCuatroLetras para almacenar el resultado de la funcion
+		String secuenciaCuatroLetras[] = new String[0];
+		
+		// Creamos el scanner
+		Scanner reader = new Scanner(System.in);
+		
+		// Pedimos al usuario que introduzca la palabra
+		System.out.println("Introduzca una palabra");
+		palabra = reader.nextLine();
+		
+		// Llamamos a la funcion y almacenamos su resultado
+		secuenciaCuatroLetras = secuenciaCuatroLetras(palabra);
+		
+		// Imprimimos el array
+		System.out.println(Arrays.toString(secuenciaCuatroLetras));
+		
 
 	}
 	
 	static String[] secuenciaCuatroLetras(String palabra) {
 		
-		String secuencia[] = new String[0];
+		// Creamos el array de cadenas donde almacenaremos las secuencias de 4 letras
+		String secuencia[];
 		
+		// Creamos la variable longitudArrayPrevia que nos permite calcular la longitud del array
+		double longitudArrayPrevia = ((double) palabra.length() / 4);
+		
+		// Creamos la variable longitudArray donde almacenaremos la longitud final del array
+		int longitudArray = 0;
+		
+		// Si la longitud del array previa tiene decimales la redondeamos
+		if (longitudArrayPrevia - (int)longitudArrayPrevia > 0) {
+			longitudArray = (int) (longitudArrayPrevia + 1);
+		}
+		
+		// Asignamos la longitud del array de cadenas
+		secuencia = new String[longitudArray];
+		
+		// Devolvemos el array de cadenas
 		return secuencia;
 	}
 }
